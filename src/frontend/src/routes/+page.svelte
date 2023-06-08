91e13978d563acfb46cac49297213f663e741a6e
Shourya <shourya.eaga.09@gmail.com>
2023-06-08 17:32:17 +0000

    fix

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import {onMount} from 'svelte'
     import {browser} from '$app/environment'
     import Todo from '$lib/Todo.svelte'
-    import Settings from $'lib/Settings.svelte';
+    import Settings from '$lib/Settings.svelte';
 
     import apiUrl from '$lib/appConfig'
     const api_url = apiUrl.apiUrl
