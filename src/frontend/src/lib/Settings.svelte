2381ff3dcf08cb608b9217370ca12bbcf06e0779
Shourya <shourya.eaga.09@gmail.com>
2023-06-08 17:39:25 +0000

    fixes

--- src/frontend/src/lib/Settings.svelte
+++ src/frontend/src/lib/Settings.svelte
@@ -20,32 +20,38 @@
         <h3>Theme</h3>
         {/if}
     </header>
-    <aside>
-        <nav>
-            <ul>
-                <li><a href="#account-settings" on:click={accountPage}>Account</a></li>
-                <li><a href="#theme" on:click={themePage}>Theme</a></li>
-            </ul>
-        </nav>
-    </aside>
     <div class="container">
-        {#if page === "account"}
-            <h2>Change username</h2>
-            WIP
-            <hr>
-            <h2>Change email</h2>
-            WIP
-            <hr>
-            <h2>Reset password</h2>
-            WIP
-            <hr>
-        {:else}
-            <h2>Select Theme</h2>
-            <ul role="listbox">
-                <li><a href="#" data-theme-switcher="auto">Auto</a></li>
-                <li><a href="#" data-theme-switcher="light">Light</a></li>
-                <li><a href="#" data-theme-switcher="dark">Dark</a></li>
-            </ul>
-        {/if}
+        <aside>
+            <nav>
+                <ul>
+                    <li><a href="#account-settings" on:click={accountPage}>Account</a></li>
+                    <li><a href="#theme" on:click={themePage}>Theme</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <div class="container">
+            {#if page === "account"}
+                <h2>Change username</h2>
+                WIP
+                <hr>
+                <h2>Change email</h2>
+                WIP
+                <hr>
+                <h2>Reset password</h2>
+                WIP
+                <hr>
+            {:else}
+                <h2>Select Theme</h2>
+                <details role="list">
+                    <ul role="listbox">
+                        <li><a href="#" data-theme-switcher="auto">Auto</a></li>
+                        <li><a href="#" data-theme-switcher="light">Light</a></li>
+                        <li><a href="#" data-theme-switcher="dark">Dark</a></li>
+                    </ul>
+                </details>
+                
+            {/if}
+        </div>
     </div>
+    
 </article>
